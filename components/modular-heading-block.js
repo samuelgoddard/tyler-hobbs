a1700b3b123fc93ab38ccc6ee2e9a270b401aaa7
samuelgoddard <hello@samgoddard.co.uk>
2023-11-20 11:50:55 +0000

    heading widths

--- components/modular-heading-block.js
+++ components/modular-heading-block.js
@@ -2,7 +2,7 @@ import BodyRich from "./body-rich";
 
 export default function ModularHeadingBlock({ text }) {
   return text && (
-    <h2 className="text-4xl lg:text-5xl xl:text-6xl w-[90%] lg:w-[75%] max-w-6xl mb-0">
+    <h2 className="text-4xl lg:text-5xl xl:text-6xl w-[90%] lg:w-[80%] mb-0">
       <BodyRich content={text} />
     </h2>
   )
